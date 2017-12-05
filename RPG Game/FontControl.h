ed60b3b62ec1d2c51a54d89e0566bacae9cdb7ce
Chris <sk8rdude619@hotmail.co.uk>
2017-12-05 17:49:24 +0000

    Included pdb files so that we can debug sf:: stuff (that I found in one of the sfml folders)

---
+++ RPG Game/FontControl.h
@@ -0,0 +1,11 @@
+#ifndef FONTCONTROL_H
+#define FONTCONTROL_H
+class FontControl
+{
+public:
+	sf::Text newTextElement(sf::Text textElement, sf::Font font);
+private:
+	sf::Font arial;
+protected:
+};
+#endif
