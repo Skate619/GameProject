4c75cdaf11f66cfaef6ade0572aa2e1a9cbcf63c
Chris <sk8rdude619@hotmail.co.uk>
2017-12-11 23:12:22 +0000

    Added an actual folder structure

--- RPG Game/FontControl.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.h"
-
-sf::Text FontControl::newTextElement(sf::Text textElement, sf::Font font)
-{
-	textElement.setFont(font);
-	textElement.setCharacterSize(20);
-	textElement.setColor(sf::Color(255, 255, 255));
-	return textElement;
-}
---
+++ RPG Game/HeaderFiles/Game.h
@@ -0,0 +1,47 @@
+#ifndef GAME_H
+#define GAME_H
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+#include <sstream>
+using namespace std;
+const int MAX_BULLETS = 3;
+
+class Game
+{
+public:
+	Game(sf::RenderWindow* app);
+	sf::RenderWindow* GameWindow;
+	void Init();
+	void Draw();
+	void UserInput();
+	void Update();
+	void Play();
+	void SetName(sf::RenderWindow* app, string myString);
+	void SetupFont(sf::Text textElement, sf::Font font);
+	string GetText(sf::Event event, string myString);
+	string InputName(sf::RenderWindow* app);
+	void Reverse(string myString);
+	void FizzBuzz();
+protected:
+	sf::Clock clock;
+
+private:
+	sf::Event event;
+
+	sf::Text help;
+	sf::Text genericThing;
+	sf::Text playerName;
+
+	sf::Font arial;
+
+	bool Enter = false;
+	bool name = true;
+
+	float helpWidth = 0;
+	float inputWidth = 0;
+
+	string myString;
+};
+#endif
